3e418cbcedc731651a7e1e8a1e3bf013b7632d5f
christian <christian962c@yahoo.com.br>
2021-11-03 19:01:30 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div >
+    <>
+    <NavBar/>
+    <div className = "container">
       <h1 className="tet-primary">Olá Mundo!</h1>
+      <DataTable/>
     </div>
+    <Footer/>
+  </>
   );
 }
 
